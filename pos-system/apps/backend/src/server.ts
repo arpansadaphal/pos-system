11db55b898ff6581f1d16123800938e79b2876e9
arpan4848 <arpansworkspace@gmail.com>
2026-04-24 09:29:18 +0000

    initial backend setup

---
+++ pos-system/apps/backend/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("POS System API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
